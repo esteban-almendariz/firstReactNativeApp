743d8d5e3ae42e7e41e94f8c3c9cb9975d54069b
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2024-01-03 23:21:40 -0600

    still learning flexbox and added usestate to list all goals on screen

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
 
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [goalList, setGoalList] = useState([])
+
+  const goalChangeHandler = (enteredText) => {
+      setEnteredGoalText(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setGoalList(prevState => [
+      ...prevState,
+      enteredGoalText
+    ])
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput 
+          style={styles.textInput}
           placeholder='Your course goal!'
+          onChangeText={goalChangeHandler}
         />
         <Button 
           title='Add Goal'
+          onPress={addGoalHandler}
         />
       </View>
-      <View>
-        <Text>List of goals...</Text>
-      </View>
-      <View>
-        <Text>1</Text>
-      </View>
-      <View>
-        <Text>2</Text>
+      <View style={styles.goalsContainer}>
+        {goalList.map((goal,index) => <Text key={index} >{goal}</Text>)}
       </View>
     </View>
   );
@@ -27,9 +40,22 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    padding: 20,
+    marginTop: 40
   },
   inputContainer: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    paddingVertical: 50,
+    borderBottomWidth: .2,
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderColor: "gray",
+    width: "70%",
+    borderWidth: 1,
+    padding: 10,
+  },
+  goalsContainer: {
+    marginTop: 25
   }
 });
